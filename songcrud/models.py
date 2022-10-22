627e478f4b5c98fb5f66d11b2fe7c3c0ef354167
Mastermind <adebowaleadam0@gmail.com>
2022-10-22 08:40:19 -0700

    Model update

---
+++ songcrud/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Artiste(models.Model):
+    first_name = models.CharField()
+    last_name = models.CharField
+    age = models.IntegerField()
+
+
+class Song(models.Model):
+    title = models.TextField()
+    date = models.DateField()
+    released = models.BooleanField()
+    likes = models.ManyToManyField('self', null = True)
+    artiste_id =models.ForeignKey(Artiste)
+
+
+class Lyrics(models.Model):
+    content = models.TextField()
+    song_id = models.ForeignKey(Song)
